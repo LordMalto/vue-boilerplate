2987722de13a7e00e5b15b3e8b6354e303f4756c
Malte <malteschubert28816@gmail.com>
2025-02-08 15:18:37 +0100

    Added a Footer component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import AppFooter from "@/components/AppFooter.vue";
 </script>
 
 <template>
+  <head>
+    <title>VueJS Boilerplate</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="VueJS Boilerplate" />
+    <meta name="keywords" content="vuejs, vue, vue3, boilerplate, tailwind, tailwindcss, supabase, auth" />
+    <meta name="author" content="Malte S" />
+    <link rel="icon" href="./assets/logo.svg" />
+  </head>
   <RouterView />
+  <AppFooter />
 </template>
-
 <style scoped>
 </style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <footer class="bg-gray-900 text-white p-4 sticky">
+    <div class="flex justify-around pb-4">
+      <ul>
+        <li class="m-2"><RouterLink to="#" class="hover:underline">Link 1</RouterLink></li>
+        <li class="m-2"><RouterLink to="#" class="hover:underline">Link 2</RouterLink></li>
+        <li class="m-2"><RouterLink to="#" class="hover:underline">Link 3</RouterLink></li>
+      </ul>
+      <ul>
+        <li class="m-2"><RouterLink to="#" class="hover:underline">Link 4</RouterLink></li>
+        <li class="m-2"><RouterLink to="#" class="hover:underline">Link 5</RouterLink></li>
+        <li class="m-2"><RouterLink to="#" class="hover:underline">Link 6</RouterLink></li>
+      </ul>
+      <ul>
+        <li class="m-2"><RouterLink to="#" class="hover:underline">Link 7</RouterLink></li>
+        <li class="m-2"><RouterLink to="#" class="hover:underline">Link 8</RouterLink></li>
+        <li class="m-2"><RouterLink to="#" class="hover:underline">Link 9</RouterLink></li>
+      </ul>
+
+    </div>
+    <div class="border-t border-gray-700 pt-4 text-center">
+      &copy; Malte S.
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
